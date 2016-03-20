f7699107bf3080e242109585fb4a58b333d7459a
code2333 <467279984@qq.com>
2016-03-20 23:15:58 +0800

    second commit

---
+++ for/for/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  for
+//
+//  Created by 罗邦杰 on 16/3/20.
+//  Copyright © 2016年 罗邦杰. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    for (int i=0;i<10; i++) {
+        cout<<"123"<<endl;
+    }
+    return 0;
+}
