9cb6ea43f4ae2073e405cb87d87a150d3d7dc8f7
code2333 <467279984@qq.com>
2016-03-20 22:53:53 +0800

    first commit

---
+++ dsa/dsa/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  dsa
+//
+//  Created by 罗邦杰 on 16/3/20.
+//  Copyright © 2016年 罗邦杰. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    for(int i=0;i<10;i++)
+    {
+    cout << "Hello, World!\n";
+    }
+    return 0;
+}
